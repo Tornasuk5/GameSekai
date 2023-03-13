feda04beff77143277b8441aca7713dffbada840
Tornasuk5 <tornasuk5@hotmail.com>
2023-03-13 13:26:22 +0100

    Version 13-03-2023

--- game/models.py
+++ game/models.py
@@ -9,6 +9,7 @@ class Game(models.Model):
     platform = models.CharField(max_length=70)
     developer = models.CharField(max_length=50)
     date_release = models.CharField(max_length=10)
-    img = models.TextField()
+    cover = models.TextField()
+    wallpaper = models.TextField()
     trailer = models.TextField()
     val = models.FloatField()
